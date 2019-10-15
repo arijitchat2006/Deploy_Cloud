3a1e188305de5fe9fbc4c854dce823131e5657f6
Arijit Chatterjee (arijicha) <arijicha@cisco.com>
2019-10-15 14:54:37 +0530

    committing the travis.yml & deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t pribhask/multi-client:latest -t pribhask/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t pribhask/multi-server:latest -t pribhask/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t pribhask/multi-worker:latest -t pribhask/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t arijitchat2006/multi-client:latest -t arijitchat2006/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t arijitchat2006/multi-server:latest -t arijitchat2006/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t arijitchat2006/multi-worker:latest -t arijitchat2006/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push pribhask/multi-client:latest
-docker push pribhask/multi-server:latest
-docker push pribhask/multi-worker:latest
+docker push arijitchat2006/multi-client:latest
+docker push arijitchat2006/multi-server:latest
+docker push arijitchat2006/multi-worker:latest
 
-docker push pribhask/multi-client:$SHA
-docker push pribhask/multi-server:$SHA
-docker push pribhask/multi-worker:$SHA
+docker push arijitchat2006/multi-client:$SHA
+docker push arijitchat2006/multi-server:$SHA
+docker push arijitchat2006/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=pribhask/multi-server:$SHA
-kubectl set image deployments/client-deployment client=pribhask/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=pribhask/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=arijitchat2006/multi-server:$SHA
+kubectl set image deployments/client-deployment client=arijitchat2006/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=arijitchat2006/multi-worker:$SHA
